c9a33fa8d7a6ec9d499f7555560acd4f774fe3c4
Amanda1024 <88503704+codewithamanda@users.noreply.github.com>
2021-11-09 12:42:34 -0800

    Added another access origin

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -20,7 +20,7 @@ const credentials = {
     token_uri: 'https://oauth2.googleapis.com/token',
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
     redirect_uris: ['https://codewithamanda.github.io/meet/'],
-    javascript_origins: ['https://codewithamanda.github.io', 'http://localhost:3000'],
+    javascript_origins: ['https://codewithamanda.github.io', 'http://localhost:3000', 'http://127.0.0.1:8080'],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
@@ -49,6 +49,7 @@ module.exports.getAuthURL = async() => {
         statusCode: 200,
         headers: {
             'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': 'true'
         },
         body: JSON.stringify({
             authUrl: authUrl,
@@ -85,6 +86,7 @@ module.exports.getAccessToken = async(event) => {
                 statusCode: 200,
                 headers: {
                     'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true'
                 },
                 body: JSON.stringify(token),
             };
