00b72bd6a39171c68cc800f5e54768284ae22e87
Amanda1024 <88503704+codewithamanda@users.noreply.github.com>
2022-01-03 15:39:23 -0800

    Create NumberOfEvents.js

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+
+    return (
+      <div className='EventNumberComponent'>
+
+      <label htmlFor='numberOfEventsInput'> Number of Events on Page:
+
+      <input
+        id='numberOfEventsInput'
+        type='number'
+        className='numberInput'
+        value={this.props.eventCounter}
+       />
+       
+      </label>
+
+      </div>
+    )
+  }
+}
+
+export default NumberOfEvents;
